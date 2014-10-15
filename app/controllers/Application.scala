0c8fad1661b8126c72bbbda30808e13249e7cab8
Pascual Maestre Server <pascual.maestreserver@gmail.com>
2014-10-15 22:20:14 +0200

    Añadidas las nuevas funciones de Application.scala para interactuar con Task.scala

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -18,23 +18,41 @@ object Application extends Controller {
      "label" -> nonEmptyText
    )
   
+  //Devuelve una coleccion JSON con la lista de tareas
   def tasks = Action {
-      Ok(views.html.index(Task.all(), taskForm))
+      var json = Json.toJson(Task.all())
+      Ok(json)
    }
   
+  //Recibe el dato de la nueva tarea (descripcion) en un formulario.
+  //Debe devolver un JSON con la descripcion de la nueva tarea creada 
+  //y el codigo HTTP 201 (CREATED)  
   def newTask = Action { implicit request =>
      taskForm.bindFromRequest.fold(
        errors => BadRequest(views.html.index(Task.all(), errors)),
        label => {
-         Task.create(label)
-         Redirect(routes.Application.tasks)
+          Task.create(label)
+          var json = Json.toJson(Task.getLastTask())
+          Created(json)
        }
      )
    }
-  
+  //Borra una tarea cuo identificador se pasa en la URI. 
+  //Si la tarea no existe debe evolver un codigo HTTP 404 (NOT FOUND)
   def deleteTask(id: Long) = Action {
-    Task.delete(id)
-    Redirect(routes.Application.tasks)
+    var cons = Task.getTask(id)
+    if(cons == Nil)
+      NotFound("404 Tarea no encontrada")
+    else{
+      Task.delete(id)
+      Ok("Se ha borrado la tarea correctamente")
+    }
+  }
+
+  //Devuelve la representacion JSON de la tarea cuyo identificador se pasa en al URI
+  def searchTask(id: Long) = Action {
+    var json = Json.toJson(Task.getTask(id))
+    Ok(json)
   }
   
 }
