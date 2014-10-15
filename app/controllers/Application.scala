27474351ac4950522cd5f291e8c2e79e634ea813
Pascual Maestre Server <pascual.maestreserver@gmail.com>
2014-10-15 22:21:57 +0200

    Añadidos los import de Application.scala

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,8 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 import play.api.data._
 import play.api.data.Forms._
